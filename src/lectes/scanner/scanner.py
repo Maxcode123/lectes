b7fc4e2e508d9c9725cb3e4b9bfffdf1103ead41
Maximos Nikiforakis <nikiforos@live.co.uk>
2025-09-10 14:57:51 +0300

    Renames program to text in scanner

--- src/lectes/scanner/scanner.py
+++ src/lectes/scanner/scanner.py
@@ -7,17 +7,17 @@ from lectes.scanner.models import Token
 class Scanner:
     def __init__(self, configuration: Configuration) -> None:
         self.configuration = configuration
-        self.set_program("")
+        self.set_text("")
         self._unmatched_handler = self._handle_unmatched
 
-    def scan(self, program: str) -> Generator[Token]:
-        if len(program) == 0:
+    def scan(self, text: str) -> Generator[Token]:
+        if len(text) == 0:
             return
 
-        self.set_program(program)
+        self.set_text(text)
 
         self.current_position = 1
-        for _ in program:
+        for _ in text:
             for rule in self.configuration.rules:
                 current_string = self.current_string()
 
@@ -32,13 +32,13 @@ class Scanner:
     def set_unmatched_handler(self, handler: Callable[[str], None]) -> None:
         self._unmatched_handler = handler
 
-    def set_program(self, program: str) -> None:
-        self.program = program
+    def set_text(self, text: str) -> None:
+        self.text = text
         self.current_position = 0
         self.last_position = 0
 
     def current_string(self) -> str:
-        return self.program[self.last_position : self.current_position]
+        return self.text[self.last_position : self.current_position]
 
     @staticmethod
     def _handle_unmatched(unmatched: str) -> None:
