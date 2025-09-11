dab29c9bf92ce32f6ae41f545f6b991e39441df6
Maximos Nikiforakis <nikiforos@live.co.uk>
2025-09-11 15:57:09 +0300

    Adds logging to scanner and corrects logic

--- src/lectes/scanner/scanner.py
+++ src/lectes/scanner/scanner.py
@@ -2,13 +2,16 @@ from typing import Callable, Generator
 
 from lectes.config.models import Configuration
 from lectes.scanner.models import Token
+from lectes.scanner.logger import Logger, LogLevel
 
 
 class Scanner:
-    def __init__(self, configuration: Configuration) -> None:
+    def __init__(self, configuration: Configuration, debug: bool = False) -> None:
         self.configuration = configuration
         self.set_text("")
         self._unmatched_handler = self._handle_unmatched
+        self._debug = debug
+        self._logger = None
 
     def scan(self, text: str) -> Generator[Token]:
         if len(text) == 0:
@@ -16,15 +19,34 @@ class Scanner:
 
         self.set_text(text)
 
-        for _ in text:
+        for character in text:
+            self.logger().debug(f"character: '{character}'")
+
+            current_string = self.current_string()
+            self.logger().debug(f"current_string: '{current_string}'")
+
+            lookahead_string = self.lookahead_string()
+            self.logger().debug(f"lookahead_string: '{lookahead_string}'")
+
             for rule in self.configuration.rules:
-                current_string = self.current_string()
+                if not self._is_last_char() and rule.regex.fullmatch(lookahead_string):
+                    self.logger().debug(
+                        f"rule {rule.name} fullmatched lookahead_string: '{lookahead_string}'"
+                    )
+                    break
 
                 if match := rule.regex.search(current_string):
+                    self.logger().debug(
+                        f"rule {rule.name} matched current_string: '{current_string}'"
+                    )
+
                     if match.unmatched is not None:
                         self._unmatched_handler(match.unmatched)
+
                     yield Token(rule=rule, literal=match.string)
+
                     self.last_position = self.current_position
+                    break
 
             self.current_position += 1
 
@@ -38,6 +60,26 @@ class Scanner:
 
     def current_string(self) -> str:
         return self.text[self.last_position : self.current_position]
+
+    def lookahead_string(self) -> str:
+        return self.text[self.last_position : self.current_position + 1]
+
+    def logger(self) -> Logger:
+        if self._logger is None:
+            self._logger = self._build_logger()
+
+        return self._logger
+
+    def _build_logger(self) -> Logger:
+        logger = Logger()
+
+        if self._debug:
+            logger.set_level(LogLevel.DEBUG)
+
+        return logger
+
+    def _is_last_char(self) -> bool:
+        return self.current_position == len(self.text)
 
     @staticmethod
     def _handle_unmatched(unmatched: str) -> None:
